27299b16a17d395d89fef63c03b3f741f6d409a2
chandanaparipelli18-alt <chandanaparipelli18@gmail.com>
2026-01-09 19:50:51 +0530

    Fix dataframe selection and add stop in Streamlit app

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -23,9 +23,10 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')),col('SEARCH_ON'))
 
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list =st.multiselect(
     'Choose uo to 5 ingredients:',my_dataframe,
