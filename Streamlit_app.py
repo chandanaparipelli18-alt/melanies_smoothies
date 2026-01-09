6081217aedc1dc42c5d80f04b63073a6ae254e86
chandanaparipelli18-alt <chandanaparipelli18@gmail.com>
2026-01-09 20:09:17 +0530

    Comment out dataframe display and stop
    Comment out dataframe display and stop calls

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -30,8 +30,8 @@ st.stop()
 #Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 
 
